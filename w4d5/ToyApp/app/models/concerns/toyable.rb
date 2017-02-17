2febb3e338be377e19358f7063cd91cc66c35dde
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-02-17 05:01:52 -0800

    homework w4d5

---
+++ w4d5/ToyApp/app/models/concerns/toyable.rb
@@ -0,0 +1,12 @@
+module Toyable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :toys, as: :toyable
+  end
+
+  def receive_toy(name)
+    toy = Toy.find_or_create_by(name: name)
+    Toy.create(name: toy.name, toyable_id: self.id, toyable_type: self.class)
+  end
+end
---
+++ w4d5/ToyApp/app/models/toy.rb
@@ -0,0 +1,5 @@
+class Toy < ActiveRecord::Base
+  belongs_to :toyable, polymorphic: true
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :toyable }
+end
