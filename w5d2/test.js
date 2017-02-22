579f63ece99218691be8fa11a2288bb9c0f82b13
App Academy Student <student@appacademy.io>
2017-02-22 09:24:30 -0800

    pahse 1

---
+++ w5d2/test.js
@@ -0,0 +1,7 @@
+function madLib(verb, adj, noun) {
+  console.log(`We shall ${verb.toUppercase()} the ${adj.toUppercase()} ${noun.toUppercase()}`)
+}
+
+function isSubstring(searchString, subString) {
+  return searchString.includes(subString);
+}
