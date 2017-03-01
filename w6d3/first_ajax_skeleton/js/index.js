00fe44aa4a0766e9a96c25f71a98f2f528349b4e
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-02-28 22:56:59 -0800

    first ajax

--- w6d3/first_ajax_skeleton/js/index.js
+++ w6d3/first_ajax_skeleton/js/index.js
@@ -1,5 +1,16 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  type: "GET",
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success: function(data) {
+    console.log(data.weather[0]);
+    console.log(data.weather[1]);
+  },
+  error: function() { console.log("error"); }
+
+});
 
 // Add another console log here, outside your AJAX request
+console.log("end of the file");
