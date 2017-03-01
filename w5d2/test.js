7ffde341dbb8c8e76530e2635597f55b3d09566e
Khalil Nasirov <khalil.nasirov@gmail.com>
2017-02-28 22:30:51 -0800

    rename and move files

--- w5d2/test.js
+++
@@ -1,7 +0,0 @@
-function madLib(verb, adj, noun) {
-  console.log(`We shall ${verb.toUppercase()} the ${adj.toUppercase()} ${noun.toUppercase()}`)
-}
-
-function isSubstring(searchString, subString) {
-  return searchString.includes(subString);
-}
